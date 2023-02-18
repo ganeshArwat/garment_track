e4a8e3eb6b1e230bf57e6de0f0ee3fcd969bbcfd
ganeshitd <111874702+ganeshitd@users.noreply.github.com>
2023-02-18 10:35:12 +0530

    database migration done

--- application/helpers/create_table_helper.php
+++ application/helpers/create_table_helper.php
@@ -20,6 +20,24 @@ if (!function_exists('app_settings_table_qry')) {
     }
 }
 
+if (!function_exists('auth_token_table_qry')) {
+    function auth_token_table_qry()
+    {
+        $qry = "CREATE TABLE IF NOT EXISTS `auth_token` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `created_by` int(11) NOT NULL,
+            `created_date` datetime NOT NULL,
+            `modified_by` int(11) NOT NULL,
+            `modified_date` datetime NOT NULL,
+            `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1.Active 2.Inactive 3.Trash',
+            `responce_data` text COLLATE utf8_unicode_ci NOT NULL,
+            `authtoken` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+            `tokenexpiry` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+        return $qry;
+    }
+}
 if (!function_exists('company_bank_table_qry')) {
     function company_bank_table_qry()
     {
@@ -94,6 +112,7 @@ if (!function_exists('company_master_table_qry')) {
     }
 }
 
+
 if (!function_exists('custom_validation_field_table_qry')) {
     function custom_validation_field_table_qry()
     {
@@ -113,6 +132,69 @@ if (!function_exists('custom_validation_field_table_qry')) {
         return $qry;
     }
 }
+
+
+if (!function_exists('document_mapping_table_qry')) {
+    function document_mapping_table_qry()
+    {
+        $qry = "CREATE TABLE IF NOT EXISTS `document_mapping` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `status` tinyint(1) NOT NULL DEFAULT 1,
+    `module_type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1:customer;2:shipper;3:pickup_address;4:other_address;5:docket shipper',
+    `module_id` int(11) NOT NULL,
+    `doc_type_id` int(11) NOT NULL,
+    `doc_no` varchar(255) NOT NULL,
+    `doc_name` varchar(255) NOT NULL,
+    `doc_page1` varchar(255) NOT NULL,
+    `doc_page2` varchar(255) NOT NULL,
+    `created_date` datetime NOT NULL,
+    `created_by` int(11) NOT NULL,
+    `modified_date` datetime NOT NULL,
+    `modified_by` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+        return $qry;
+    }
+}
+
+if (!function_exists('login_log_table_qry')) {
+    function login_log_table_qry()
+    {
+        $qry = "CREATE TABLE IF NOT EXISTS `login_log` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+            `status` tinyint(1) NOT NULL DEFAULT 1,
+            `login_date` datetime NOT NULL,
+            `user_id` int(11) NOT NULL,
+            `user_type` tinyint(1) NOT NULL,
+            `ip_address` int(11) NOT NULL,
+            PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+        return $qry;
+    }
+}
+
+if (!function_exists('media_attachment_table_qry')) {
+    function media_attachment_table_qry()
+    {
+        $qry = "CREATE TABLE IF NOT EXISTS `media_attachment` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `status` tinyint(1) NOT NULL DEFAULT 1,
+    `module_id` int(11) NOT NULL,
+    `module_type` int(11) NOT NULL COMMENT '1:docket;2:docket_item',
+    `media_key` varchar(255) NOT NULL,
+    `media_path` varchar(255) NOT NULL,
+    `third_party_url` varchar(255) NOT NULL COMMENT 'image,pdf url of api response',
+    `created_date` datetime NOT NULL,
+    `created_by` int(11) NOT NULL,
+    `modified_date` datetime NOT NULL,
+    `modified_by` int(11) NOT NULL,
+    `created_mode` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1:created in software;2:api created',
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+        return $qry;
+    }
+}
+
 if (!function_exists('module_setting_table_qry')) {
     function module_setting_table_qry()
     {
@@ -150,3 +232,16 @@ if (!function_exists('setting_data_table_qry')) {
     }
 }
 
+if (!function_exists('user_permission_map_table_qry')) {
+    function user_permission_map_table_qry()
+    {
+        $qry = "CREATE TABLE IF NOT EXISTS `user_permission_map` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `status` tinyint(1) NOT NULL DEFAULT 1,
+    `user_id` int(11) NOT NULL,
+    `permission_id` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+        return $qry;
+    }
+}
--- application/helpers/database_manage_helper.php
+++ application/helpers/database_manage_helper.php
@@ -7,7 +7,7 @@ if (!function_exists('create_company_database')) {
         if ($company_id > 0) {
             $ci->load->database();
             $ci->load->dbutil();
-            $company_db_name = "garment_track_company_" . $company_id;
+            $company_db_name = "garment_track_theme_company_" . $company_id;
             if (!$ci->dbutil->database_exists($company_db_name)) {
                 $ci->load->dbforge();
                 $ci->dbforge->create_database($company_db_name);
@@ -26,9 +26,9 @@ if (!function_exists('create_all_table')) {
         $session_data = isset($_SESSION['admin_user']) ? $_SESSION['admin_user'] : array();
 
         if (isset($_GET['cron_company']) && $_GET['cron_company'] > 0) {
-            $database_name = "garment_track_company_" . $_GET['cron_company'];
+            $database_name = "garment_track_theme_company_" . $_GET['cron_company'];
         } else  if (isset($session_data['is_restrict']) && $session_data['is_restrict'] == 2) {
-            $database_name = "garment_track_company_" . $session_data['com_id'];
+            $database_name = "garment_track_theme_company_" . $session_data['com_id'];
         } else {
             $database_name = "garment_track_theme";
         }
@@ -72,7 +72,7 @@ if (!function_exists('create_all_table')) {
 
 
         $table_list = array(
-            'app_settings', 'company_bank', 'company_master', 'custom_validation_field', 'module_setting', 'setting_data'
+            'app_settings', 'auth_token', 'company_bank', 'company_master', 'custom_validation_field', 'document_mapping', 'login_log', 'media_attachment', 'module_setting', 'setting_data', 'user_permission_map'
         );
 
         if ($return_type == 'get_table') {
