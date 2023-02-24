aaf98da4745eaf49c3533829ea21f45d749da7b4
sudipitd <111874147+sudipitd@users.noreply.github.com>
2023-02-24 19:20:31 +0530

    Update company_list.php

--- application/modules/company/views/company_list.php
+++ application/modules/company/views/company_list.php
@@ -12,20 +12,29 @@ $session_data = $this->session->userdata('admin_user');
                 <div class="row">
                     <div class="col-sm-3">
                         <!-- text input -->
-                        <div class="form-group">
-                            <label>Company Name</label>
-                            <input type="hidden" value="<?php echo isset($get_data['company_id']) ? $get_data['company_id'] : ''; ?>" id="company_id" name="company_id" />
-                            <input placeholder="SEARCH HERE..." class="form-control" id="company_search_name" type="text" value="<?php echo isset($get_data['company_id']) && isset($all_company[$get_data['company_id']]) ? $all_company[$get_data['company_id']]['company_name'] : ''; ?>" />
+                        <!-- <div class="form-group"> -->
+                        <!-- <label>Company Name</label> -->
+                        <!-- <input type="hidden" value="<?php echo isset($get_data['company_id']) ? $get_data['company_id'] : ''; ?>" id="company_id" name="company_id" /> -->
+                        <!-- <input placeholder="Search" class="form-control me-2" id="company_search_name" type="text" value="<?php echo isset($get_data['company_id']) && isset($all_company[$get_data['company_id']]) ? $all_company[$get_data['company_id']]['company_name'] : ''; ?>" /> -->
+                        <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> -->
+                        <!-- <span class="input-group-text"><i class="tf-icons bx bx-search"></i></span> -->
+                        <!-- <input type="text" class="form-control" placeholder="Search..." /> -->
 
+                        <!-- </div> -->
+                        <div class="input-group" style="padding-top:23px;">
+                            <span class="input-group-text"><i class="tf-icons bx bx-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Search..." />
                         </div>
                     </div>
 
                     <div class="col-sm-9">
                         <!-- text input -->
-                        <div class="form-group" style="padding-top:23px;">
-                            <input id="btn-search" tabindex="4" type="submit" class="btn bg-olive" value="Search">
-                            <a tabindex="6" class="btn btn-info" title="View All Company" href="<?php echo site_url('company/show_list'); ?>"><i class="fa fa-newspaper-o" aria-hidden="true"></i>&nbsp;RESET</a>
-                            <a tabindex="7" class="btn btn-danger" title="Add Company" href="<?php echo site_url('company/add'); ?>"><i class="fa fa-plus-square" aria-hidden="true"></i>&nbsp;Add</a>
+                        <!-- <button id="btn-search" class="btn btn-outline-primary" tabindex="7" type="submit">Search</button> -->
+
+                        <div class="form-group" style="padding-top:23px; padding-bottom: 11px;margin-left:437px">
+
+                            <a tabindex="7" class="btn btn-info" title="View All Company" href="<?php echo site_url('company/show_list'); ?>"><i aria-hidden="true"></i>&nbsp;RESET</a>
+                            <a tabindex="7" class="btn btn-danger" title="Add Company" href="<?php echo site_url('company/add'); ?>"><i aria-hidden="true"></i>&nbsp;Add</a>
 
                             <!-- <a tabindex="7" class="btn btn-danger" id="backup_btn" href="#"><i class="fa fa-paper" aria-hidden="true"></i>&nbsp;BACKUP ALL COMPANY</a> -->
 
@@ -44,59 +53,67 @@ $session_data = $this->session->userdata('admin_user');
 </div>
 <div class="col-12">
     <!-- /.box -->
-
-    <div class="box">
-        <div class="box-header with-border">
-            <span>Total No. Of Company: <?php echo isset($total) ? $total : 0; ?></span>
-            <span>Total Display: <?php echo isset($list) ? count($list) : 0; ?></span>
+    <div class="card-header">
+        <div class="box">
+            <span style="font-weight:bold">Total No. Of Company: <?php echo isset($total) ? $total : 0; ?></span>
+            <span style="font-weight:bold">Total Display: <?php echo isset($list) ? count($list) : 0; ?></span>
             <div class="float-right">
                 <?php echo $this->pagination->create_links(); ?>
             </div>
         </div>
+    </div>
+
+    <div class="box">
+
         <!-- /.box-header -->
-        <div class="box-body no-padding">
-            <table class="table table-striped table-responsive">
-                <tbody>
-                    <tr>
-                        <th style="width: 10px">Sr.No.</th>
-                        <th>Name</th>
-                        <!-- <th>Code</th> -->
-                        <th>No. Of Logins</th>
-                        <th>Expiry Date</th>
-                        <th>SEF URL</th>
-                        <th>Created Date</th>
-                        <th>Action</th>
-                    </tr>
-                    <?php
-                    $cnt = $offset + 1;
+        <div class="card">
 
-                    if (isset($list) && is_array($list) && count($list) > 0) {
-                        foreach ($list as $key => $value) { ?>
-                            <tr class="<?php echo $value['status'] == 2 ? 'inactive_row' : ''; ?>">
-                                <td><?php echo $cnt; ?></td>
-                                <td><?php echo $value['company_name']; ?></td>
-                                <!-- <td><?php echo $value['company_code']; ?></td> -->
-                                <td><?php echo $value['login_count']; ?></td>
-                                <td><?php echo get_format_date(DATE_FORMAT, ($value['expiry_date'])); ?></td>
-                                <td><?php echo $value['sef_url']; ?></td>
+            <div class="table-responsive text-nowrap">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th style="width: 10px">Sr.No.</th>
+                            <th>Name</th>
+                            <!-- <th>Code</th> -->
+                            <th>No. Of Logins</th>
+                            <th>Expiry Date</th>
+                            <th>SEF URL</th>
+                            <th>Created Date</th>
+                            <th style="text-align: center;">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <?php
+                        $cnt = $offset + 1;
 
-                                <td><?php echo get_format_date(DATETIME_FORMAT, $value['created_date']); ?></td>
+                        if (isset($list) && is_array($list) && count($list) > 0) {
+                            foreach ($list as $key => $value) { ?>
+                                <tr class="<?php echo $value['status'] == 2 ? 'inactive_row' : ''; ?>">
+                                    <td><?php echo $cnt; ?></td>
+                                    <td><?php echo $value['company_name']; ?></td>
+                                    <!-- <td><?php echo $value['company_code']; ?></td> -->
+                                    <td><?php echo $value['login_count']; ?></td>
+                                    <td><?php echo get_format_date(DATE_FORMAT, ($value['expiry_date'])); ?></td>
+                                    <td><?php echo $value['sef_url']; ?></td>
 
-                                <td>
-                                    <a href="<?php echo site_url('company/edit/' . $value['id']) ?>" title="Update Company" class="edit_button">Edit</a>
+                                    <td><?php echo get_format_date(DATETIME_FORMAT, $value['created_date']); ?></td>
 
-                                    <a target="blank" href="<?php echo site_url('cron/droplet_api/db_backup/' . $value['id'] . '?cron_company=' . $value['id']) ?>" title="BACKUP Company" class="delete_button backup_company">Backup Database</a>
+                                    <td>
+                                        <a href="<?php echo site_url('company/edit/' . $value['id']) ?>" title="Update Company" class="btn btn-secondary">Edit</a>
 
-                                </td>
-                            </tr>
-                    <?php
-                            $cnt++;
+                                        <a target="blank" href="<?php echo site_url('cron/droplet_api/db_backup/' . $value['id'] . '?cron_company=' . $value['id']) ?>" title="BACKUP Company" class="btn btn-info">Backup Database</a>
+
+                                    </td>
+                                </tr>
+                        <?php
+                                $cnt++;
+                            }
                         }
-                    }
-                    ?>
+                        ?>
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <!-- /.box-body -->
     </div>
